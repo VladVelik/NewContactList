831ff956f5e01ab884d0d03039070ab00e331913
VladVelik <sosinvladik@yandex.ru>
2023-02-26 18:27:17 +0300

    Initial Commit

---
+++ NewContactList/NewContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewContactListApp.swift
+//  NewContactList
+//
+//  Created by Sosin Vladislav on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
